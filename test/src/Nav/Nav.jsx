5b2705f36acecba6e987aa0aab43976c69ca5570
EvgenijN <parapa199@mail.ru>
2025-04-02 01:33:24 +0300

    NavLink

--- test/src/Nav/Nav.jsx
+++ test/src/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styleNav from "./Nav.module.css";
 
 export function Nav(){
@@ -5,11 +6,22 @@ export function Nav(){
         <>
         <nav className={styleNav.nav}>
           <ul>
-            <li className={styleNav.nav__item}><a href="/main">Profile</a></li>
-            <li className={styleNav.nav__item}><a href="/dialog">Messages</a></li>
-            <li className={styleNav.nav__item}><a href="/news">News</a></li>
-            <li className={styleNav.nav__item}><a href="/music">Music</a></li>
-            <li className={styleNav.nav__item}><a href="/settings">Settings</a></li>
+            <li className={styleNav.nav__item}>
+              <NavLink to="/main" className={({ isActive }) =>isActive ? `${styleNav.nav__item} ${styleNav.activeLink}` : styleNav.nav__item}
+              >Profile</NavLink>
+              </li>
+            <li className={styleNav.nav__item}>
+              <NavLink to="/dialog" className={({ isActive }) =>isActive ? `${styleNav.nav__item} ${styleNav.activeLink}` : styleNav.nav__item}>Messages</NavLink>
+              </li>
+            <li className={styleNav.nav__item}>
+              <NavLink to="/news" className={({ isActive }) =>isActive ? `${styleNav.nav__item} ${styleNav.activeLink}` : styleNav.nav__item}>News</NavLink>
+              </li>
+            <li className={styleNav.nav__item}>
+              <NavLink to="/music" className={({ isActive }) =>isActive ? `${styleNav.nav__item} ${styleNav.activeLink}` : styleNav.nav__item}>Music</NavLink>
+              </li>
+            <li className={styleNav.nav__item}>
+              <NavLink to="/settings" className={({ isActive }) =>isActive ? `${styleNav.nav__item} ${styleNav.activeLink}` : styleNav.nav__item}>Settings</NavLink>
+              </li>
           </ul>
         </nav>
         </>
